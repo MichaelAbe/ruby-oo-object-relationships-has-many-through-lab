388a8c3a26dba28539b848606c0aafc2e2455c3f
MichaelAbe <michael.k.abe@gmail.com>
2020-11-24 22:03:48 -0500

    commit tree 6a8ef611c2cf3748526208f027d1f783df96619a

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        Song.all.map{|song| song.artist}
+    end
+end
